c85d8f9142191dea2357c21473a1e32130f0f2e6
Shashank Prasanna <shashankprasanna1@gmail.com>
2025-07-18 17:18:20 -0400

    Add tests for fast transcription

--- tests/transcription_test.py
+++ tests/transcription_test.py
@@ -50,3 +50,43 @@ class TestTranscriptionClass:
             print(final_response)
             assert final_response["status"] == "success"
             assert final_response["filename"] == f"obama.{output_format}"
+
+class TestFastTranscriptionClass:
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_files(self, request):
+        with open("tests/test_media/obama.mp3", "rb") as file:
+            audio_response = client.post("/uploadmedia", files={"file": file})
+            print("Audio upload response:", audio_response.json())
+            request.cls.file_ID = audio_response.json()["fileID"]
+        yield
+        print("Cleaning up uploaded files...")
+        client.post(f"/deletemedia?fileID={request.cls.file_ID}")
+
+    @pytest.mark.parametrize("model", supported_models)
+    @pytest.mark.parametrize("output_format", ["srt", "vtt", "txt", "ass"])
+    def test_fast_transcription(self, model, output_format):
+        print(f"Testing fast transcription with model {model}, output format {output_format}")
+        data = {
+            "filename": "obama.mp3",
+            "fileID": self.file_ID,
+            "model": model,
+            "output_format": output_format
+        }
+        with client.websocket_connect("/transcribe-fast") as websocket:
+            websocket.send_json(data)
+            while True:
+                response = websocket.receive_json()
+                print(response)
+                if response["status"] == "progress":
+                    print("Progress:", response)
+                elif response["status"] == "error":
+                    print("Error during transcription:", response["message"])
+                    if "Transcription timeout" in response["message"]:
+                        final_response = response
+                        break
+                else:
+                    final_response = response
+                    break
+            print(final_response)
+            assert final_response["status"] == "success"
+            assert final_response["filename"] == f"obama.{output_format}"
